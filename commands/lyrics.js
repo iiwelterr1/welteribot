57cf05fe3db337e11dd8f1a331593352f886cf90
iiwelterr1 <70782217+iiwelterr1@users.noreply.github.com>
2020-09-05 02:43:46 +0300

    Update lyrics.js

--- commands/lyrics.js
+++ commands/lyrics.js
@@ -3,19 +3,19 @@ const lyricsFinder = require("lyrics-finder");
 
 module.exports = {
   name: "lyrics",
-  aliases: ["ly"],
-  description: "Get lyrics for the currently playing song",
+  aliases: ["lr", "كلمات"],
+  description: "كلمات الاغنية اللي تشتغل ( انقليزية (",
   async execute(message) {
     const queue = message.client.queue.get(message.guild.id);
-    if (!queue) return message.channel.send("There is nothing playing.").catch(console.error);
+    if (!queue) return message.channel.send("`ما فيه شي يشتغل .`").catch(console.error);
 
     let lyrics = null;
 
     try {
       lyrics = await lyricsFinder(queue.songs[0].title, "");
-      if (!lyrics) lyrics = `No lyrics found for ${queue.songs[0].title}.`;
+      if (!lyrics) lyrics = `ما لقيت كلمات  ${queue.songs[0].title}.`;
     } catch (error) {
-      lyrics = `No lyrics found for ${queue.songs[0].title}.`;
+      lyrics = `ما لقيت كلمات ${queue.songs[0].title}.`;
     }
 
     let lyricsEmbed = new MessageEmbed()
