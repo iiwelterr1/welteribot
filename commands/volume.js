f0b51d122ee58a5fa69f3acadb5dc403e7e74105
iiwelterr1 <70782217+iiwelterr1@users.noreply.github.com>
2020-09-05 02:56:50 +0300

    Update volume.js

--- commands/volume.js
+++ commands/volume.js
@@ -2,23 +2,23 @@ const { canModifyQueue } = require("../util/EvobotUtil");
 
 module.exports = {
   name: "volume",
-  aliases: ["v"],
-  description: "Change volume of currently playing music",
+  aliases: ["vol", "صوت"],
+  description: "تغيير الصوت ",
   execute(message, args) {
     const queue = message.client.queue.get(message.guild.id);
 
-    if (!queue) return message.reply("There is nothing playing.").catch(console.error);
+    if (!queue) return message.reply("`ما فيه اغنية تشتغل .!`").catch(console.error);
     if (!canModifyQueue(message.member))
-      return message.reply("You need to join a voice channel first!").catch(console.error);
+      return message.reply("`لازم تكون في الروم !`").catch(console.error);
 
-    if (!args[0]) return message.reply(`🔊 The current volume is: **${queue.volume}%**`).catch(console.error);
-    if (isNaN(args[0])) return message.reply("Please use a number to set volume.").catch(console.error);
+    if (!args[0]) return message.reply(`🔊 الصوت الان : **${queue.volume}%**`).catch(console.error);
+    if (isNaN(args[0])) return message.reply("`اختر رقم لتغيير الصوت`.").catch(console.error);
     if (parseInt(args[0]) > 100 || parseInt(args[0]) < 0)
-      return message.reply("Please use a number between 0 - 100.").catch(console.error);
+      return message.reply("`اختر رقم من 1 الى 100`").catch(console.error);
 
     queue.volume = args[0];
     queue.connection.dispatcher.setVolumeLogarithmic(args[0] / 100);
 
-    return queue.textChannel.send(`Volume set to: **${args[0]}%**`).catch(console.error);
+    return queue.textChannel.send(`الصوت الان : **${args[0]}%**`).catch(console.error);
   }
 };
