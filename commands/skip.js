683e5e438d747f80252cf3dc5e7959b5bb365f33
iiwelterr1 <70782217+iiwelterr1@users.noreply.github.com>
2020-09-05 02:56:26 +0300

    Update skip.js

--- commands/skip.js
+++ commands/skip.js
@@ -2,8 +2,8 @@ const { canModifyQueue } = require("../util/EvobotUtil");
 
 module.exports = {
   name: "skip",
-  aliases: ["s"],
-  description: "Skip the currently playing song",
+  aliases: ["sk", "سكيب", "سكب"],
+  description: "تخطي الاغنية الحاليه",
   execute(message) {
     const queue = message.client.queue.get(message.guild.id);
     if (!queue)
