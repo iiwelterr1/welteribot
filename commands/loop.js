c99f4905a456b3c2a153c4c1812ee1d5026e299f
iiwelterr1 <70782217+iiwelterr1@users.noreply.github.com>
2020-09-05 02:43:11 +0300

    Update loop.js

--- commands/loop.js
+++ commands/loop.js
@@ -2,17 +2,17 @@ const { canModifyQueue } = require("../util/EvobotUtil");
 
 module.exports = {
   name: "loop",
-  aliases: ['l'],
-  description: "Toggle music loop",
+  aliases: ['كرر'],
+  description: "تكرار الاغنية ",
   execute(message) {
     const queue = message.client.queue.get(message.guild.id);
-    if (!queue) return message.reply("There is nothing playing.").catch(console.error);
+    if (!queue) return message.reply("`ما فيه اغنية تشتغل .!`").catch(console.error);
     if (!canModifyQueue(message.member)) return;
 
     // toggle from false to true and reverse
     queue.loop = !queue.loop;
     return queue.textChannel
-      .send(`Loop is now ${queue.loop ? "**on**" : "**off**"}`)
+      .send(`التكرار الحين : ${queue.loop ? "**مشتغل**" : "**مقفل**"}`)
       .catch(console.error);
   }
 };
